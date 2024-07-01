7d1a9fbdd4ea2a7af276ec2e8adf0741db73fad8
Codedys <oberegeraldjackson@gmail.com>
2024-07-01 15:30:30 +0300

    wrote all the data into an excell file

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
-import requests,bs4,re
+import requests,bs4,re,time
 from urllib.parse import urlparse ,urlunparse,parse_qs,urlencode
+from openpyxl import Workbook
 
+#  start of variable assosiated with the  write to excel, write to excel should be written on its own file later
+x = []
+wb = Workbook()
+sheet = wb.active
+
+pc= 1
+prc=2
+lc=3
+
+def write_to_excel(data):
+    row = 1
+    for item in data:
+        sp = item.split(',')
+        cur = {
+            "product":sp[0],
+            "price": "XXX",
+            "link":"XXX"
+        }
+        x.append(cur)
+        sheet.cell(row,pc).value= cur['product']
+        sheet.cell(row,prc).value= cur['price']
+        sheet.cell(row,lc).value = cur['link']
+        row += 1
+    wb.save('Laptops.xlsx')
+
+
+# end of the variables
 
 filtered_data=[]
 url=''
@@ -26,6 +54,7 @@ while iterator <= 50:
     
     req = requests.get(url)
     req.raise_for_status()
+    print('Scrapping page ........................................................')
 
     passed_html = bs4.BeautifulSoup(req.text,'html.parser')
     scrapped_data = passed_html.select('h3.name')
@@ -35,10 +64,11 @@ while iterator <= 50:
         if mo:
             ram_value = int(mo.group(1))
             if ram_value >=8:
-                filtered_data.append(product)
+                
+                filtered_data.append(product.getText())
     iterator +=1
 
-print(filtered_data)
+write_to_excel(filtered_data)
 
 
 
